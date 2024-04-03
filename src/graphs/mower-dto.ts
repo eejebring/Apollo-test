b0a302a4f9478b48f06f0ab67c774ec5ed3e42bd
eejebring <dev@ejebring.com>
2024-04-03 15:19:05 +0200

    dto descriptions

--- src/graphs/mower-dto.ts
+++ src/graphs/mower-dto.ts
@@ -1,14 +1,14 @@
 import {Field, ID, ObjectType} from "type-graphql"
 import "reflect-metadata"
 
-@ObjectType()
+@ObjectType({description: "This object represents a real-world mower"})
 export class MowerDTO {
-	@Field(() => ID, {name: "id"})
+	@Field(() => ID, {name: "id", description: "The id of the mower"})
 	id: string
 
-	@Field(() => String)
+	@Field(() => String, {description: "The name of the mower"})
 	name: string
 
-	@Field(() => String)
+	@Field(() => String, {description: "The owner of the mower"})
 	owner: string
 }
