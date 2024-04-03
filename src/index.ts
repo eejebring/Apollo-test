e9525a7f99e7afb5040ba55c6d65285614acb717
eejebring <dev@ejebring.com>
2024-04-03 16:58:17 +0200

    Express & subscription framework

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import {ApolloServer} from "@apollo/server"
-import {startStandaloneServer} from "@apollo/server/standalone"
 import {buildTypeDefsAndResolvers} from "type-graphql"
 import mongoose from "mongoose"
 import {MowerDto} from "./graphs/mower-dto"
 import {MowerResolver} from "./resolvers/mower-resolver"
 import {mowerRepo} from "./repositories/mower-repo"
+import {createServer} from "http"
+
+import {ApolloServerPluginDrainHttpServer} from "@apollo/server/plugin/drainHttpServer"
+
+import {WebSocketServer} from "ws"
+
+import {useServer} from "graphql-ws/lib/use/ws"
+import {makeExecutableSchema} from "@graphql-tools/schema"
+import express from "express"
+import cors from "cors"
+import {expressMiddleware} from "@apollo/server/express4"
 
 const db_url = "mongodb://localhost:27017"
 await mongoose.connect(db_url)
@@ -20,17 +30,47 @@ const {typeDefs, resolvers} = await buildTypeDefsAndResolvers({
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
+
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+// Create an Express app and HTTP server; we will attach both the WebSocket
+// server and the ApolloServer to this HTTP server.
+const app = express()
+
+const httpServer = createServer(app)
+
+// Create our WebSocket server using the HTTP server we just set up.
+const wsServer = new WebSocketServer({
+	server: httpServer,
+	path: "/subscriptions",
 })
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const sandbox_url = (await startStandaloneServer(server, {
-	listen: {port: 4000},
-})).url
+// Save the returned server's info so we can shutdown this server later
+const serverCleanup = useServer({schema}, wsServer)
 
-console.log(`🚀  Sandbox ready at: ${sandbox_url}`)
+// Set up ApolloServer.
+const apolloServer = new ApolloServer({
+	schema,
+	plugins: [
+		ApolloServerPluginDrainHttpServer({httpServer}),
+		{
+			async serverWillStart() {
+				return {
+					async drainServer() {
+						await serverCleanup.dispose()
+					},
+				}
+			},
+		},
+	],
+})
+
+await apolloServer.start()
+app.use("/graphql", cors<cors.CorsRequest>(), express.json(), expressMiddleware(apolloServer))
+
+const PORT = 4000
+
+// Now that our HTTP server is fully set up, we can listen to it.
+httpServer.listen(PORT, () => {
+	console.log(`Server is now running on http://localhost:${PORT}/graphql`)
+})
